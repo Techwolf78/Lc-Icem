ebe3e746312cba7aa171907f99c0ab087bfd226e
Gaurav <122665090+Gaurav4344@users.noreply.github.com>
2025-09-15 15:46:17 +0530

    Update Sidebar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Sidebar from "./SideBar";
+// import AdminNavbar from "./Admin/AdminNavbar";
+// import AdminSidebar from "./Admin/AdminSidebar";
 
 const Layout = ({ children }) => {
   return (
